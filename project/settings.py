27328a6e68f30cb589984651611b02bd77d0f8e3
giftedmatrix <ebutejonah386@gmail.com>
2022-09-04 16:08:56 +0100

    heroku setup

--- project/settings.py
+++ project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from decouple import config
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast = bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
