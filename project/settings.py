feb0e25f359781cde9c67cc3a8e593ced1bef4f1
giftedmatrix <ebutejonah386@gmail.com>
2022-09-08 12:05:14 +0100

    removed allauth backend

--- project/settings.py
+++ project/settings.py
@@ -157,7 +157,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    'allauth.account.auth_backends.AuthenticationBackend',  
+    #'allauth.account.auth_backends.AuthenticationBackend',  
 ]
 
 # Default primary key field type
@@ -176,7 +176,7 @@ CORS_REPLACE_HTTPS_REFERER      = True
 HOST_SCHEME                     = "https://"
 SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT             = True
-#SESSION_COOKIE_SECURE           = True
+SESSION_COOKIE_SECURE           = True
 CSRF_COOKIE_SECURE              = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
 SECURE_HSTS_SECONDS             = 1000000
