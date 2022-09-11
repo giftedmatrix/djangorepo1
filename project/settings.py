609ea1b3b2bae045438108ede22bb0744247b1a5
giftedmatrix <ebutejonah386@gmail.com>
2022-09-11 21:41:32 +0100

    bought domain name

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://www.nettflixxclone.herokuapp.com']
+ALLOWED_HOSTS = ['www.netchlix.com']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
