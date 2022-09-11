705ddb5724f18b90d6eb06cb131d6fb055995a8b
giftedmatrix <ebutejonah386@gmail.com>
2022-09-11 21:50:11 +0100

    .

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.netchlix.com']
+ALLOWED_HOSTS = ['netchlix.com']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -186,17 +186,6 @@ load_dotenv()
 CSRF_TRUSTED_ORIGINS = [
     'https://netchlix.com'
 ]
-
-
-CORS_REPLACE_HTTPS_REFERER      = True
-HOST_SCHEME                     = "https://"
-SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT             = True
-SESSION_COOKIE_SECURE           = True
-CSRF_COOKIE_SECURE              = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
-SECURE_HSTS_SECONDS             = 1000000
-SECURE_FRAME_DENY               = True
 
 
 django_heroku.settings(locals())
