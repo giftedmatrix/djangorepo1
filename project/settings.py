1b6d2d6dea2076b86b2601442701de3c246acd2f
giftedmatrix <ebutejonah386@gmail.com>
2022-11-14 10:42:16 +0100

    debug

--- project/settings.py
+++ project/settings.py
@@ -35,9 +35,9 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 # Generally avoid wildcards(*). However since Heroku router provides hostname validation it is ok
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['nettflixxclone.herokuapp.com']
+ALLOWED_HOSTS = ["*",'nettflixxclone.herokuapp.com']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
