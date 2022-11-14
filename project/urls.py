f8d07fdce9e3519e111a30e6582459b25641e81d
giftedmatrix <ebutejonah386@gmail.com>
2022-11-14 11:24:29 +0100

    ladida

--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,7 @@ try:
         path('',include('core.urls',namespace='core')),
         path('accounts/', include('allauth.urls')),
     ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
     if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
